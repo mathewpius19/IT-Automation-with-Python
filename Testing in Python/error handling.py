d7af2b083654553b649ac2686a3c15fe42fa0674
Mathew <57001761+mathewpius19@users.noreply.github.com>
2020-05-14 00:45:12 +0530

    Delete error handling.py

--- Testing in Python/error handling.py
+++
@@ -1,8 +0,0 @@
-def validate_user(username, minlen):
-    if minlen<3:
-        raise ValueError("Value should be atleast 1")
-    if len(username) < minlen:
-        return False
-    if not username.isalnum():
-        return False
-    return True
